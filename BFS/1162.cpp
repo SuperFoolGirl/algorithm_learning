7ba3135d7e011429693a48ded2ee06ff34b080b6
SuperFoolGirl <1831177023@qq.com>
2025-01-21 22:28:57 +0800

    2025寒假

--- BFS/1162.cpp
+++ BFS/1162.cpp
@@ -98,12 +98,76 @@ int main()
     return 0;
 }
 
-
-
-
-
 // 视频讲解
+// 思路还是找不能染色的，然后反向输出
+// 我的灵机一动确实不错 但找不能染色的方法错了
+// 我当时还开了一个痕迹数组，其实想多了
+// 按照bfs模板，三句continue(边界、vis、map)可以得到一个事实：永远走不到1
+// 而需要染色的区域，是在1的基础上，才能走的到的
+// 所以，到不了1，就到不了染色区。就是这么简单。痕迹数组纯纯招笑
+// 在这个逻辑基础上，处理一下边界条件即可
+// 这里借鉴求行列式的一个伟大方法：加边法
+// 把外面一圈都写成0，这样无脑传0,0首坐标
+// 否则就要像我一样，先遍历所有的0，然后水坑思路
+#include <iostream>
+#include <queue>
+using namespace std;
+#define maxn 35
 
+int n;
+int map[maxn][maxn] = {0};
+bool vis[maxn][maxn] = {false};
+queue<pair<int, int>> q;
+int dx[] = {-1, 0, 1, 0};
+int dy[] = {0, 1, 0, -1};
+
+void bfs(int x, int y)
+{
+    q.push({x, y});
+    vis[x][y] = true;
+
+    while (!q.empty())
+    {
+        auto t = q.front();
+        q.pop();
+
+        for (int i = 0; i < 4; i++)
+        {
+            int a = t.first + dx[i];
+            int b = t.second + dy[i];
+
+            if (a < 0 || a > n + 1 || b < 0 || b > n + 1) // 注意加边带来的边界处理，全部往外扩一层
+                continue;
+            if (vis[a][b] == true)
+                continue;
+            if (map[a][b] == 1)
+                continue;
+
+            vis[a][b] = true;
+            q.push({a, b});
+        }
+    }
+}
+
+int main()
+{
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+        for (int j = 1; j <= n; j++)
+            cin >> map[i][j];
+    bfs(0, 0); // 加边发力了
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
+            if (map[i][j] == 0 && vis[i][j] == false)
+                map[i][j] = 2;
+            cout << map[i][j] << ' ';
+        }
+        cout << endl;
+    }
+    return 0;
+}
 
 
 
