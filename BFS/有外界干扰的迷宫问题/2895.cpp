0772961e41e4eb7ef3a8a28f135dcffa64ac1ba7
SuperFoolGirl <1831177023@qq.com>
2025-01-22 12:29:50 +0800

    2025寒假

--- BFS/有外界干扰的迷宫问题/2895.cpp
+++ BFS/有外界干扰的迷宫问题/2895.cpp
@@ -1,4 +1,4 @@
-// 自己写的 尽力了 只有一个点没过
+// 自己写的 尽力了
 // 理所应当地写得很丑陋...
 
 // 简单说一下思路
@@ -27,7 +27,6 @@ using namespace std;
 int m;
 int dx[] = {-1, 0, 1, 0};
 int dy[] = {0, 1, 0, -1};
-int map[maxn][maxn] = {0};
 queue<pair<int, int>> q;
 
 // 大多数情况下，vis和dist没必要同时存在。dist==-1就是vis==false
@@ -65,7 +64,7 @@ int bfs(int x, int y)
             int aroundX = shot[i].x + dx[j];
             int aroundY = shot[i].y + dy[j];
 
-            if (aroundX < 0 || aroundX > 300 || aroundY < 0 || aroundY > 300)
+            if (aroundX < 0 || aroundX > 301 || aroundY < 0 || aroundY > 301) // 这地方让卡了！焦土会再扩一圈！但0那个点无妨，改成-1的话直接数组越界了
                 continue;
             vis[aroundX][aroundY] = true;
         }
@@ -90,7 +89,7 @@ int bfs(int x, int y)
                 int aroundX = shot[i].x + dx[j];
                 int aroundY = shot[i].y + dy[j];
 
-                if (aroundX < 0 || aroundX > 300 || aroundY < 0 || aroundY > 300)
+                if (aroundX < 0 || aroundX > 301 || aroundY < 0 || aroundY > 301)
                     continue;
 
                 vis[aroundX][aroundY] = true;
@@ -103,7 +102,7 @@ int bfs(int x, int y)
             int nextX = head.first + dx[i];
             int nextY = head.second + dy[i];
 
-            if (nextX < 0 || nextX > 300 || nextY < 0 || nextY > 300)
+            if (nextX < 0 || nextY < 0) // 注意人的范围
                 continue;
             if (vis[nextX][nextY] == true)
                 continue;
@@ -140,12 +139,93 @@ int main()
         {
             int aroundX = shot[i].x + dx[j];
             int aroundY = shot[i].y + dy[j];
-            if (aroundX < 0 || aroundX > 300 || aroundY < 0 || aroundY > 300)
+            if (aroundX < 0 || aroundX > 301 || aroundY < 0 || aroundY > 301)
                 continue;
             boomedMap[aroundX][aroundY] = true;
         }
     }
 
+    cout << bfs(0, 0);
+    return 0;
+}
+
+
+
+
+// 视频解法：并不是只开一张爆炸地图，而是储存爆炸时间
+// 考虑一个问题：同一个点可能被多次炸，要记录最早时间
+// 这样连结构体都不需要了，我那段代码最恶心的点就是结构体及其排序，还引入一个idx，可读性非常差
+// 然后看当前时刻是否符合下一步安全地点
+// 体会一下自己的思维缺陷
+
+#include <iostream>
+#include <queue>
+#include <cstring>
+#include <algorithm>
+#define maxn 310
+using namespace std;
+
+int m;
+// 拓展方向数组 加上原点。遍历时炸弹取5，人取4
+int dx[] = {-1, 0, 1, 0, 0};
+int dy[] = {0, 1, 0, -1, 0};
+int boomed[maxn][maxn];
+int dist[maxn][maxn];
+queue<pair<int, int>> q;
+
+int bfs(int x, int y)
+{
+    q.push({x, y});
+    dist[x][y] = 0;
+
+    while (!q.empty())
+    {
+        auto head = q.front();
+        q.pop();
+
+        for (int i = 0; i < 4; i++) // 人取4
+        {
+            int nextX = head.first + dx[i];
+            int nextY = head.second + dy[i];
+
+            if (nextX < 0 || nextY < 0)
+                continue;
+            if (dist[nextX][nextY] != -1)
+                continue;
+            if (dist[head.first][head.second] + 1 >= boomed[nextX][nextY]) // 注意这个判断条件
+                continue;
+
+            q.push({nextX, nextY});
+            dist[nextX][nextY] = dist[head.first][head.second] + 1;
+
+            // 判断是否到达安全地点
+            if (boomed[nextX][nextY] == 0x3f3f3f3f) // 如果走到没被炸的地方
+                return dist[nextX][nextY];
+        }
+    }
+    return -1;
+}
+
+int main()
+{
+    cin >> m;
+    // 体会下memset的作用。统一初始化为-1/0/0x3f，下面对数组赋值。如果遍历到-1/0/0x3f，就意味着这个点没被赋值过
+    memset(dist, -1, sizeof dist);
+    // 使用 memset 初始化时，0x3f 会将每个字节设置为 0x3f，因此 int 类型的变量会被初始化为 0x3f3f3f3f
+    memset(boomed, 0x3f, sizeof boomed); // 这里初始化为无穷大 是为了配合下方min赋值
+    for (int i = 0; i < m; i++)
+    {
+        int x, y, t;
+        cin >> x >> y >> t;
+        for (int j = 0; j < 5; j++) // 炸弹取5
+        {
+            int aroundX = x + dx[j];
+            int aroundY = y + dy[j];
+            if (aroundX < 0 || aroundX > 301 || aroundY < 0 || aroundY > 301)
+                continue;
+            boomed[aroundX][aroundY] = min(boomed[aroundX][aroundY], t);
+        }
+    }
     cout << bfs(0, 0);
     return 0;
 }
