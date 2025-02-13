aeb93df454e7b9680ed050c67dfff542aa9638be
SuperFoolGirl <164290520+SuperFoolGirl@users.noreply.github.com>
2025-02-13 21:26:25 +0800

    fal24

--- 题目/高精度乘法.c
+++ 题目/高精度乘法.c
@@ -14,42 +14,30 @@ int main()
     int len = lena + lenb; // 不用再加1
 
     for (int i = lena - 1, j = 0 ; i >= 0; i--, j++)
-    {
         a[j] = input1[i] - '0';
-    }
+
     for (int i = lenb - 1, j = 0; i >= 0; i--, j++)
-    {
         b[j] = input2[i] - '0';
-    }
 
     for (int i = 0; i < lena; i++)
-    {
         for (int j = 0; j < lenb; j++)
-        {
             c[i + j] += a[i] * b[j]; // 注意+=号 以及c的下标（如果ij从1开始 下标要改成i+j-1）
-        }
-    }
+
+    // c数组都被标记了，无法再直接赋值，只能加等
     for (int i = 0; i < len; i++)
     {
-        c[i + 1] += c[i] / 10; // +=  i+1
-        c[i] %= 10;            // %=  i
+        c[i + 1] += c[i] / 10;
+        c[i] %= 10;
     }
 
     while (!c[len - 1])
-    {
         len--;
-    }
 
     if (len >= 1) // 防止乘积为0
-    {
         for (int i = len - 1; i >= 0; i--)
-        {
             printf("%d", c[i]);
-        }
-    }
     else
-    {
         printf("0");
-    }
+        
     return 0;
 }
