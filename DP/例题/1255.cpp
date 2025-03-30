9b31a0f004b7ee1c179bb6bd203075caa9d02c01
SuperFoolGirl <164290520+SuperFoolGirl@users.noreply.github.com>
2025-03-30 16:06:35 +0800

    2025春

--- DP/例题/1255.cpp
+++ DP/例题/1255.cpp
@@ -30,3 +30,68 @@ int main()
     std::cout << dfs(n);
     return 0;
 }
+
+
+
+// 高精度写法
+// 参考lanqiao的10425题处的高精度写法
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <map>
+using namespace std;
+const int N = 5010;
+
+int n;
+map<int, string> dp;
+
+string add(string &a, string &b)
+{
+    vector<int> x;
+    vector<int> y;
+    reverse(a.begin(), a.end());
+    reverse(b.begin(), b.end());
+
+    for (auto c : a)
+        x.push_back(c - '0');
+    for (auto c : b)
+        y.push_back(c - '0');
+
+    // 反转要还原，以后还要用
+    reverse(a.begin(), a.end());
+    reverse(b.begin(), b.end());
+
+    // 高精度加高精度，要保证两个数的长度一致
+    // 扩容为0，避免越界访问
+    int longer = max(x.size(), y.size());
+    x.resize(longer, 0);
+    y.resize(longer, 0);
+
+    int carry = 0;
+    // 这里不需要另外开一个res数组，直接在x或y的基础上加就行
+    for (int i = 0; i < longer; i++)
+    {
+        carry += x[i] + y[i];
+        x[i] = carry % 10;
+        carry /= 10;
+    }
+    if (carry)
+        x.push_back(carry);
+
+    string res;
+    for (auto i : x)
+        res.push_back(i + '0');
+    reverse(res.begin(), res.end());
+    return res;
+}
+
+int main()
+{
+    cin >> n;
+    dp[1] = "1";
+    dp[2] = "2";
+    for (int i = 3; i <= n; i++)
+        dp[i] = add(dp[i - 1], dp[i - 2]);
+    cout << dp[n];
+    return 0;
+}
