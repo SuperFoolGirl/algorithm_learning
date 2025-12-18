18b9f6cfeb2314f96330f60989aa3ba421f52b21
Kita <mahoshojoriruru@gmail.com>
2025-12-18 18:24:05 +0800

    251218

--- 基础/3.搜索与图论/二叉树/常用算法/前中后序建树问题/根据中序和前序输出后序.cpp
+++ 基础/3.搜索与图论/二叉树/常用算法/前中后序建树问题/根据中序和前序输出后序.cpp
@@ -2,7 +2,6 @@
 #include <cstring>
 #include <iostream>
 
-
 using namespace std;
 
 void buildPost(string in, string pre) {
@@ -15,11 +14,11 @@ void buildPost(string in, string pre) {
     }
 
     char root = pre[0];
-    int mid = in.find(root);
+    int mid = in.find(root);    // 左子树的长度就是mid
 
-    // 左
+    // 左：in是左根右，pre是根左右，左子树长度为mid，二者分别从0/1开始取mid个字符作为左子树
     buildPost(in.substr(0, mid), pre.substr(1, mid));
-    // 右
+    // 右：in是左根右，pre是根左右，对于二者来说，右子树都是从mid+1开始到结尾
     buildPost(in.substr(mid + 1), pre.substr(mid + 1));
     // 根
     cout << root;
--- 基础/3.搜索与图论/二叉树/常用算法/前中后序建树问题/根据中序和后序输出前序.cpp
+++ 基础/3.搜索与图论/二叉树/常用算法/前中后序建树问题/根据中序和后序输出前序.cpp
@@ -2,7 +2,6 @@
 #include <cstring>
 #include <iostream>
 
-
 using namespace std;
 
 void buildPre(string in, string post) {
@@ -19,9 +18,9 @@ void buildPre(string in, string post) {
 
     // 根
     cout << root;
-    // 左
+    // 左：in是左根右，post是左右根，左子树长度为mid，二者都是从0开始取mid个字符作为左子树
     buildPre(in.substr(0, mid), post.substr(0, mid));
-    // 右
+    // 右：对于in，右子树是从mid+1开始到结尾；对于post，左子树结尾下标是mid-1，右子树是从mid开始到倒数第2个字符
     buildPre(in.substr(mid + 1), post.substr(mid, in.size() - mid - 1));
 }
 
