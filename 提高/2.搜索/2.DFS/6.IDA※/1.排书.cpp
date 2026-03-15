532dc6c98aaba16af922876f0dc3cc1cdd2c1daf
Kita <mahoshojoriruru@gmail.com>
2026-03-15 19:16:29 +0800

    260315

--- 提高/2.搜索/2.DFS/6.IDA※/1.排书.cpp
+++ 提高/2.搜索/2.DFS/6.IDA※/1.排书.cpp
@@ -1,4 +1,4 @@
-// 搜索过程：
+// 搜索过程很像区间DP
 // 书总长为n，1~n
 // 枚举移动书目的长度i，长度为i的段共n-i+1个
 // 拿出去这i本书后，剩余n-i本书，产生n-i+1个空位；除去原先的位置，剩余n-i个空位
@@ -13,8 +13,6 @@
 // tot统计所有不正确的后继，每次移动会修复三个；因此启发函数为tot/3上取整
 // 考虑到cpp的除法下取整，改写为(tot+2)/3
 
-// 这里还用到了迭代加深的记忆化搜索
-
 #include <algorithm>
 #include <cstring>
 #include <iostream>
@@ -26,7 +24,60 @@ const int N = 15;
 
 int n;
 int q[N];
-int w[5][N];    // 用于迭代加深的记忆化搜索
+int w[5][N];  // 记录depth层的状态，回退时直接覆盖即可
+
+int f() {
+    int tot = 0;
+    for (int i = 0; i + 1 < n; i++) {
+        if (q[i] + 1 != q[i + 1]) {
+            tot++;
+        }
+    }
+    return (tot + 2) / 3;
+}
+
+bool dfs(int u, int depth) {
+    // 迭代加深+IDA*
+    if (u + f() > depth) {
+        return false;
+    }
+    // 出口判断——全部有序
+    // 这里居然是用启发函数来判断的，很有意思
+    if (f() == 0) {
+        return true;
+    }
+
+    // 枚举长度
+    for (int len = 1; len <= n; len++) {
+        // 枚举起点
+        for (int l = 0; l + len - 1 < n; l++) {
+            int r = l + len - 1;
+            // 枚举空位，只往后放+放到k的后面去
+            for (int k = r + 1; k < n; k++) {
+                memcpy(w[depth], q, sizeof q);
+
+                // 将区间[l,r]的书目移动到k的后面去：
+                // 1.将原先[r+1,k]前移，起点为l
+                // 2.将原先[l,r]的部分顺着上面继续往后放
+                int y = l;  // x从w中获取原数据，y更新写入q
+                for (int x = r + 1; x <= k; x++, y++) {
+                    q[y] = w[depth][x];
+                }
+                for (int x = l; x <= r; x++, y++) {
+                    q[y] = w[depth][x];
+                }
+
+                if (dfs(u + 1, depth)) {
+                    return true;
+                }
+
+                memcpy(q, w[depth], sizeof q);
+            }
+        }
+    }
+
+    return false;
+}
 
 int main() {
     ios::sync_with_stdio(false);
@@ -40,7 +91,17 @@ int main() {
             cin >> q[i];
         }
 
-        int depth = 0;
-         
+        int depth = 1;
+        while (depth < 5 && !dfs(0, depth)) {
+            depth++;
+        }
+
+        if (depth >= 5) {
+            cout << "5 or more" << endl;
+        } else {
+            cout << depth << endl;
+        }
     }
+
+    return 0;
 }
---
+++ 提高/3.图论/1.单源最短路/1.建图方式/1.热浪.cpp
@@ -0,0 +1,19 @@
+// 非负边权单源最短路，这里用spfa
+
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+
+using namespace std;
+
+const int N = 2510, M = 12410;
+
+int n, m, S, T;
+int h[N], e[M], ne[M], w[M], idx;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    
+    
+}
